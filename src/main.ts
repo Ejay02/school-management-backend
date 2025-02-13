5f2eaa24de4eebc749e81dea62b517fb5ac64608
Ejay02 <e.jae02@gmail.com>
2025-02-13 14:17:53 +0100

    feat: enable CORS for the application

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+  });
+
   // Use raw middleware for Stripe webhook endpoint
   app.use(
     '/webhook',
